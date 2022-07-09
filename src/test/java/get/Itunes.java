1311125efc91b1e60ac6cd1fedf49c5a234222ff
Hazel1813 <hazelcebel@gmail.com>
2022-07-08 20:53:26 -0700

    push API

---
+++ src/test/java/get/Itunes.java
@@ -0,0 +1,23 @@
+package get;
+
+import org.junit.Test;
+
+public class Itunes {
+    @Test
+    public void itunesTest1(){
+        /*
+        1. Define/find a valid/coreect URL to send an HTTP request to
+        2. Define a proper HTTP Method
+        3. Define quesry string parameters (IF NEEDED)  -> optional
+        4. Define header parameters
+        5. Send/Execute a request
+         */
+
+
+
+    }
+
+
+
+
+}
