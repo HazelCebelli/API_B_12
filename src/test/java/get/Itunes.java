6dbd6d27bf238c06e14fbc687b4ec09aa182504f
Hazel1813 <hazelcebel@gmail.com>
2022-07-08 22:26:49 -0700

    push API

--- src/test/java/get/Itunes.java
+++ src/test/java/get/Itunes.java
@@ -9,13 +9,19 @@ public class Itunes {
         /*
         1. Define/find a valid/coreect URL to send an HTTP request to
         2. Define a proper HTTP Method
-        3. Define quesry string parameters (IF NEEDED)  -> optional
+        3. Define query string parameters (IF NEEDED)  -> optional
         4. Define header parameters
         5. Send/Execute a request
          */
         // https://itunes.apple.com/search
 
-        RestAssured.baseURI="https://itunes.apple.com/search";
+        RestAssured.baseURI="https://itunes.apple.com/search";  // this is just assignment. It is reassignment
+        RestAssured.given().header("Accept","application/json")   //prerequisite
+                .queryParam("term","michael+jackson")
+                .queryParam("limit","1")
+                .when().get()   //action
+                .then().statusCode(200); // validation
+
 
 
 
---
+++ src/test/java/get/Petstore.java
@@ -0,0 +1,26 @@
+package get;
+
+import io.restassured.RestAssured;
+import org.junit.Test;
+
+public class Petstore {
+    //Create anew test Method
+    //Send a get request to "https://petstore.swagger.io/v2/pet/10567"
+    //Validate response status code equals to 200
+
+    @Test
+    public void getPetStoreTest1() {
+
+        RestAssured.baseURI = "https://petstore.swagger.io";  // this is just assignment. It is reassignment
+        RestAssured.basePath= "v2/pet/10567";
+
+        RestAssured.given()
+                .header("Accept", "application/json") .log().all()  // if we put it after header it will print the REQUEST
+                .when()
+                .get()   //action
+                .then()
+                .statusCode(200); // validation  // we can put .log().all() to print the same thing from postman
+
+    }
+
+}
