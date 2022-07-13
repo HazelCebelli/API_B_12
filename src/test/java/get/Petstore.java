d4eae109f2d93fdf3677ddca501b04d7c026d395
Hazel1813 <hazelcebel@gmail.com>
2022-07-12 20:51:10 -0700

    push API

--- src/test/java/get/Petstore.java
+++ src/test/java/get/Petstore.java
@@ -20,6 +20,18 @@ public class Petstore {
                 .then()
                 .statusCode(404); // validation  // !!!!!we can put .log().all() to print the same thing from postman
 
+        Map<String, Integer> map = new HashMap<String, Integer> ();
+        for (String s:strings) {
+
+            if (!map.containsKey(s)) {  // first time we've seen this string
+                map.put(s, 1);
+            }
+            else {
+                int count = map.get(s);
+                map.put(s, count + 1);
+            }
+        }
+        return map;
     }
 
 }
