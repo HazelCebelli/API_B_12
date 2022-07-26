09679d288c1f5e03969dc67f5f4191af38e4f446
Hazel1813 <hazelcebel@gmail.com>
2022-07-25 23:56:38 -0700

    push API

--- src/test/java/get/Deserialization.java
+++ src/test/java/get/Deserialization.java
@@ -43,9 +43,9 @@ public class Deserialization {
         // to be able to use it we need to CAST it!!! oTHERWISE WEW ILL GET ClassCastException.
 
         System.out.println(("Catogory Map : "+ categoryMap));
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("1");
+
+        System.out.println("Monday");
+
 
 
 
